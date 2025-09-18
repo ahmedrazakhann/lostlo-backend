7ff115d632b4cb9fbb36cdb36d59f0c2df6d51ea
ahmedrazakhan12 <ahmedrazakhank112@gmail.com>
2025-09-18 15:21:32 +0500

    Setup DB.

--- route/v1/user.route.js
+++ route/v1/user.route.js
@@ -1,29 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../../controller/user.controller.js');
+const userController = require("../../controller/user.controller.js");
 const auth = require("../../middleware/auth");
-const validate = require('../../middleware/validate');
-const userValidation = require('../../validations/user.validation');
+const validate = require("../../middleware/validate");
+const userValidation = require("../../validations/user.validation");
 
 router
-    .route('/')
-    .post(auth('manageUsers'),validate(userValidation.createUser) ,userController.signUp)
-    .get(auth('getUsers'),validate(userValidation.getUsers) ,userController.getAllUsers);
-
+  .route("/")
+  .post(
+    auth("manageUsers"),
+    validate(userValidation.createUser),
+    userController.signUp
+  )
+  .get(
+    auth("getUsers"),
+    validate(userValidation.getUsers),
+    userController.getAllUsers
+  );
 
 router
-    .route('/:id')
-    .get(auth('getUsers'),validate(userValidation.getUser) , userController.getUserById)
-    .patch(auth('manageUsers'),validate(userValidation.updateUser) , userController.updateUser)
-    .delete(auth('manageUsers'), validate(userValidation.deleteUser) ,userController.deleteUser);
-
-
-
+  .route("/:id")
+  .get(
+    auth("getUsers"),
+    validate(userValidation.getUser),
+    userController.getUserById
+  )
+  .patch(
+    auth("manageUsers"),
+    validate(userValidation.updateUser),
+    userController.updateUser
+  )
+  .delete(
+    auth("manageUsers"),
+    validate(userValidation.deleteUser),
+    userController.deleteUser
+  );
 
 module.exports = router;
-
-
-
 
 /**
  * @swagger
